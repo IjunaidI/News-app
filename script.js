1c96f187093741b482d63ba4869ccf942d0117c3
IjunaidI <mjunaid7843@gmail.com>
2018-08-25 15:54:27 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,35 @@
 $.ajax({
     url:`https://newsapi.org/v2/top-headlines?sources=polygon&apiKey=4ba4dbcdc43341919c99b02be806ba04`,
     success: function(data){
-        document.querySelector(".n1").innerHTML = data.articles[0].title;
-        document.querySelector(".latest-n1").style.backgroundImage = `url(${data.articles[0].urlToImage})`;
-        document.querySelector(".n2").innerHTML = data.articles[1].title;
-        document.querySelector(".latest-n2").style.backgroundImage = `url(${data.articles[1].urlToImage})`;
-        document.querySelector(".n3").innerHTML = data.articles[2].title;
-        document.querySelector(".latest-n3").style.backgroundImage = `url(${data.articles[2].urlToImage})`;
-        document.querySelector(".n4").innerHTML = data.articles[3].title;
-        document.querySelector(".latest-n4").style.backgroundImage = `url(${data.articles[3].urlToImage})`;
-        document.querySelector(".n5").innerHTML = data.articles[4].title;
-        document.querySelector(".latest-n5").style.backgroundImage = `url(${data.articles[4].urlToImage})`;
+       for(i=0;i<6;i++){
+           console.log("lanat")
+           document.querySelector(".gen1").innerHTML += `
+           <div class="latest-n1" style="background-image: url(${data.articles[i].urlToImage})">
+           <div class="bg-dark">
+           <div class="name">IGN</div>
+           <div class="title n1">${data.articles[i].title}</div>
+           <button class="info1">More Info</button>
+       </div>       
+       </div>`
+       }
     }
 
 })
 $.ajax({
     url:`https://newsapi.org/v2/everything?sources=polygon&apiKey=4ba4dbcdc43341919c99b02be806ba04`,
     success: function(data){
-        document.querySelector(".p-name1").innerHTML = data.articles[10].title;
-        document.querySelector(".news1").style.backgroundImage = `url(${data.articles[10].urlToImage})`;
-        document.querySelector(".p-name2").innerHTML = data.articles[11].title;
-        document.querySelector(".news2").style.backgroundImage = `url(${data.articles[11].urlToImage})`;
-        document.querySelector(".p-name3").innerHTML = data.articles[12].title;
-        document.querySelector(".news3").style.backgroundImage = `url(${data.articles[12].urlToImage})`;
-        document.querySelector(".p-name4").innerHTML = data.articles[13].title;
-        document.querySelector(".news4").style.backgroundImage = `url(${data.articles[13].urlToImage})`;
-        document.querySelector(".p-name5").innerHTML = data.articles[14].title;
-        document.querySelector(".news5").style.backgroundImage = `url(${data.articles[14].urlToImage})`;
-        document.querySelector(".p-name6").innerHTML = data.articles[15].title;
-        document.querySelector(".news6").style.backgroundImage = `url(${data.articles[15].urlToImage})`;
-        document.querySelector(".p-name7").innerHTML = data.articles[16].title;
-        document.querySelector(".news7").style.backgroundImage = `url(${data.articles[16].urlToImage})`;
-        document.querySelector(".p-name8").innerHTML = data.articles[17].title;
-        document.querySelector(".news8").style.backgroundImage = `url(${data.articles[17].urlToImage})`;
+        for(i=7;i<17;i++){
+           console.log("lanat")
+           document.querySelector(".gen2").innerHTML += `
+           <div class="latest-n1" style="background-image: url(${data.articles[i].urlToImage})">
+           <div class="bg-dark">
+           <div class="name">IGN</div>
+           <div class="title n1">${data.articles[i].title}</div>
+           <button class="info1">More Info</button>
+       </div>       
+       </div>`
     }
-
+    }
 })
 function showTime(){
     var date = new Date();
